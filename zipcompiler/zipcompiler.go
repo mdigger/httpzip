5ce10196e8f5183b53c116d420b7f6e9c88d64e8
Dmitry Sedykh <dmitrys@xyzrd.com>
2014-08-15 17:11:39 +0400

    Добавлено сжатие CSS и JS

--- zipcompiler/zipcompiler.go
+++ zipcompiler/zipcompiler.go
@@ -3,7 +3,10 @@ package main
 import (
 	"archive/zip"
 	"flag"
+	"github.com/alexzorin/csscompress_go"
+	"github.com/dchest/jsmin"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,11 +16,15 @@ func main() {
 	log.SetFlags(0)
 	log.SetPrefix("")
 	var (
-		outFile  string
-		mimeType string
+		outFile     string
+		mimeType    string
+		cssCompress bool
+		jsCompress  bool
 	)
 	flag.StringVar(&outFile, "out", "archive.zft", "output file name")
 	flag.StringVar(&mimeType, "mime", "application/x-archive+zip", "archive mimetype")
+	flag.BoolVar(&cssCompress, "csscompress", true, "compress css files")
+	flag.BoolVar(&jsCompress, "jscompress", true, "compress javascript files")
 	flag.Parse()
 
 	archive, err := os.Create(outFile)
@@ -60,9 +67,30 @@ func main() {
 		if err != nil {
 			log.Fatalln("Error creating file in archive:", err)
 		}
-		_, err = io.Copy(stream, file)
-		if err != nil {
-			log.Fatalln("Error copying file to archive:", err)
+
+		if cssCompress && filepath.Ext(arg) == ".css" {
+			log.Printf("Compressing css %q...", arg)
+			css, err := csscompress.New(file, stream)
+			if err != nil {
+				log.Fatalln("Error creating css compressor:", err)
+			}
+			css.Minify()
+		} else if jsCompress && filepath.Ext(arg) == ".js" {
+			log.Printf("Compressing javascript %q...", arg)
+			data, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalln("Error reading javascript file:", err)
+			}
+			data, err = jsmin.Minify(data)
+			if err != nil {
+				log.Fatalln("Error minifing javascript file:", err)
+			}
+			stream.Write(data)
+		} else {
+			_, err = io.Copy(stream, file)
+			if err != nil {
+				log.Fatalln("Error copying file to archive:", err)
+			}
 		}
 		log.Println("Added", arg)
 	}
